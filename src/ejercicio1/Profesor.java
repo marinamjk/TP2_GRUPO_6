807e3f16421dbb2bbeb88a3b9dd09d26926b527f
Adriel Vazquez <adrialvaz@hotmail.com>
2025-03-31 18:09:00 -0300

    Agrego getters-setters y override toString

--- src/ejercicio1/Profesor.java
+++ src/ejercicio1/Profesor.java
@@ -9,6 +9,36 @@ public class Profesor extends Empleado {
 		super(nombre, edad);
 		this.cargo = cargo;
 		this.antiguedadDocente = antiguedadDocente;
+	}
+	
+	public Profesor() {
+		super();
+		this.cargo = "sin cargo";
+		this.antiguedadDocente = 0;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + getId() + " NOMBRE: " + getNombre() + " EDAD: " + getEdad() + " CARGO:" +
+	getCargo() + " ANTIGÜEDAD: " + getAntiguedadDocente();
 	}	
+	
+	
 	
 }
