7201dfedcdde5d935a4716f91d71f2e2a4be9639
BILBODEVE <facundotaraujo@hotmail.com>
2025-04-01 20:02:43 -0300

    Creacion, carga y visualizacion de arrayList Profesor

---
+++ src/ejercicio1/MainEjercicio1_a.java
@@ -0,0 +1,44 @@
+package ejercicio1;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class MainEjercicio1_a {
+	
+	static final int CANT_PROFESORES = 5;
+	
+	public static void main(String[] args) {
+		
+		Profesor[] vProfesor = crearVProfesores();
+		ArrayList<Profesor> listProfesor = new ArrayList<Profesor>(CANT_PROFESORES);
+		cargarArray(vProfesor, listProfesor);
+		mostrarArray(listProfesor);
+	}
+	
+	static Profesor[] crearVProfesores()
+	{
+		
+		Profesor[] vProfesor = new Profesor[CANT_PROFESORES];
+		
+		vProfesor[0] = new Profesor("Juan", 40, "Titular",12);
+		vProfesor[1] = new Profesor("Maximiliano", 26, "Ayudante de catedra",1);
+		vProfesor[2] = new Profesor("Tamara", 35, "Jefe de trabajos practicos",7);
+		vProfesor[3] = new Profesor("Angel", 50, "Ayudante",20);
+		vProfesor[4] = new Profesor("Laura", 31, "Ayudante de catedra",4);
+		
+		return vProfesor;
+	}
+	
+	static void cargarArray(Profesor[] vProfesor, ArrayList<Profesor> listProfesor) {
+		for(Profesor p : vProfesor)
+			listProfesor.add(p);
+	}
+	
+	static void mostrarArray(ArrayList<Profesor> listProfesor)
+	{
+		ListIterator<Profesor> iteratorProfesor = listProfesor.listIterator();
+		
+		while(iteratorProfesor.hasNext())
+			System.out.println(iteratorProfesor.next() + "\n") ;
+	}
+}
