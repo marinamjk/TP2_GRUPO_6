2f584886e066c92fdc629e7e6f9c0c1362a91367
Analia Leyez <124401921+AnaliaLeyez@users.noreply.github.com>
2025-04-02 19:19:39 -0300

    .

--- src/ejercicio2/mainEjercicio2.java
+++ src/ejercicio2/mainEjercicio2.java
@@ -1,18 +1,8 @@
 package ejercicio2;
 
-import java.util.ArrayList;
-
 public class mainEjercicio2 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 
-		//para completar
-			ArrayList<Producto> listProductos = new ArrayList<Producto>();
-
-			
-			for (Producto producto : listProductos) {
-				producto.mostrarInfo();
-			}
-		
-		
 	}
 }
