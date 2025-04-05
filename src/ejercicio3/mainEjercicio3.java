3a4c2c24d9c8bacce8b867c5504213500570f639
BILBODEVE <facundotaraujo@hotmail.com>
2025-04-05 13:14:14 -0300

    Creacion clase

---
+++ src/ejercicio3/mainEjercicio3.java
@@ -0,0 +1,15 @@
+package ejercicio3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class mainEjercicio3 {
+	public static void main(String[] args) {
+
+		final Set<Integer> tipoIPolideportivo = new HashSet<>();
+		
+		tipoIPolideportivo.add(ITipoInstalacion.EDIFICIO);
+		tipoIPolideportivo.add(ITipoInstalacion.INSTALACION_DEPORTIVA);
+
+	}
+}
