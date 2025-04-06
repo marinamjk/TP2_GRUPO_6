61cd6f96eddbf64b7a43c2f2f214fdc436a25a2f
Sebastian Berti <sebaberti.8@gmail.com>
2025-04-06 12:03:06 -0300

    se agrego array list e Iterator

--- src/ejercicio3/mainEjercicio3.java
+++ src/ejercicio3/mainEjercicio3.java
@@ -1,6 +1,8 @@
 package ejercicio3;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class mainEjercicio3 {
@@ -10,6 +12,27 @@ public class mainEjercicio3 {
 		
 		tipoIPolideportivo.add(ITipoInstalacion.EDIFICIO);
 		tipoIPolideportivo.add(ITipoInstalacion.INSTALACION_DEPORTIVA);
+		
+		 ArrayList<Object> listaElementos = new ArrayList<>();
+	        
+		 Polideportivo polideportivo1 = new Polideportivo("Polideportivo A", 5000.0, tipoIPolideportivo);
+	        
+	   
+	     EdificioDeOficinas edificio1 = new EdificioDeOficinas(10, 2000.0);
+	   
+	        
+	      
+	      listaElementos.add(polideportivo1);
+	      listaElementos.add(edificio1);
+	        
+	        
+	        
+	        Iterator<Object> iterator = listaElementos.iterator();
+	        
+	        while (iterator.hasNext()) {
+	            Object elemento = iterator.next();
+	            System.out.println(elemento.toString());
+	        }
 
 	}
 }
