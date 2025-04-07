f0ad835ee8acd8bdba3562ef7bd8d5ebabe7f3ee
Agus <142065648+agusdeansodev@users.noreply.github.com>
2025-04-06 22:06:03 -0300

    Update mainEjercicio2.java
    Implementacion de un tree set para mostrar los productos de forma ordenada

--- src/ejercicio2/mainEjercicio2.java
+++ src/ejercicio2/mainEjercicio2.java
@@ -3,6 +3,7 @@ package ejercicio2;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.TreeSet;
 
 public class mainEjercicio2 {
 	public static void main(String[] args) 
@@ -22,5 +23,21 @@ public class mainEjercicio2 {
 		for (Producto producto : listaProductos) {
 			producto.mostrarInfo();
 		}
+		//----------------------------Ordenar los productos en un tree set-------------------------
+		
+		TreeSet<Producto> listaOrdenadaProductos = new TreeSet<>();
+
+		listaOrdenadaProductos.add(new Fresco(new GregorianCalendar(2025,Calendar.APRIL, 11).getTime(),
+				101, new GregorianCalendar(2025, Calendar.APRIL, 6).getTime(), "Argentina"));
+		listaOrdenadaProductos.add(new ProductoCongelado(new GregorianCalendar(2026,Calendar.DECEMBER, 20).getTime(),
+				200, -18));
+		listaOrdenadaProductos.add(new ProductoRefrigerado(new GregorianCalendar(2025, Calendar.MAY, 15).getTime(),
+				150, 001234));
+
+       
+        	for (Producto producto : listaOrdenadaProductos) {
+            	producto.mostrarInfo();
+        }
+
 	}
 }
