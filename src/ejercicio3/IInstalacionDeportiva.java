d8637a18d5fe4f175799c78fbbf1db3fb4c0a5bc
Agus <142065648+agusdeansodev@users.noreply.github.com>
2025-04-08 23:21:59 -0300

    Update IInstalacionDeportiva.java
    Se comento la linea 5 ya que com el cambio del metodo a int ya no seria util

--- src/ejercicio3/IInstalacionDeportiva.java
+++ src/ejercicio3/IInstalacionDeportiva.java
@@ -2,7 +2,7 @@ package ejercicio3;
 
 public interface IInstalacionDeportiva
 {
-	public static final String INSTALACION_DEPORTIVA= "Instalación Deportiva";
+	//public static final String INSTALACION_DEPORTIVA= "Instalación Deportiva";
 	public String getTipoDeInstalacion();	
 }
 
